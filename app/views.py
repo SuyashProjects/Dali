1fb9abd7a4ad2b614c7ed743cca1acd7ed140540
SuyashProjects <suyashgarg2@gmail.com>
2018-06-25 13:31:02 +0530

    Change

--- app/views.py
+++ app/views.py
@@ -64,12 +64,12 @@ def sequence(request):
       SKU2=2
       Seq=[]
       temp=[]
+      SKU_temp=[]
       for x in range (0,Total_Order//Ratio_Sum):
         for y in range(0,Ratio1):
          Seq.append(SKU1)
         for y in range(0,Ratio2):
          Seq.append(SKU2)
-      print(Seq)
       for value in Seq:
           b=Config.objects.filter(SKU=value).values()
           temp.append(b)
