ceae22e3915a2c8b5eaff6b6e35b8b179ac0da04
SuyashProjects <suyashgarg2@gmail.com>
2018-06-25 17:52:02 +0530

    Something I Forgot

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     url(r'^$', views.form1, name='form1'),
     url(r'^form2/$', views.form2, name='form2'),
     url(r'^sequence/$', views.sequence, name='sequence'),
-    url(r'^ajax/populate/$', views.populate, name='populate'),
-    url(r'^ajax/populate_variant/$', views.populate_variant, name='populate_variant'),
-    url(r'^ajax/populate_color/$', views.populate_color, name='populate_color'),
-    url(r'^ajax/populate_tank/$', views.populate_tank, name='populate_tank'),
 ]
